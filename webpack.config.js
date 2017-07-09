61cb20efb0f506d96bf35e1200dca9b59c091ab7
Boris Kaul <localvoid@gmail.com>
2017-07-09 14:57:14 +0700

    Remove babel and uglify from build pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpack = require("webpack");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   target: "node",
@@ -12,26 +11,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["es2015", "es2016"],
-            },
-          },
-        ],
-      },
-      {
         test: /\.ts$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["es2015", "es2016"],
-            },
-          },
           {
             loader: "ts-loader",
             options: {
@@ -48,7 +30,6 @@ module.exports = {
       "__IVI_BROWSER__": false,
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJSPlugin(),
   ],
   resolve: {
     alias: {
